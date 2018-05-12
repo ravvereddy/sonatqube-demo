3ec0d9eadea5fcc0dc15e0d753d5d21e6d4d7866
reddy.raviteja46 <reddy.raviteja46@gmail.com>
2018-05-13 01:08:44 +0530

    SonarProject

---
+++ sonar-packaging-maven-plugin/travis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function configureTravis {
+  mkdir -p ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+configureTravis
+
+regular_mvn_build_deploy_analyze
