cf58daa9b5c75b8f9d68039ac99341bfe2df9142
reddy.raviteja46 <reddy.raviteja46@gmail.com>
2018-05-12 12:55:42 +0530

    main commented

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -4,9 +4,9 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-#public class App 
+public class App 
 {
-    public static void main( String[] args )
+#    public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
 
